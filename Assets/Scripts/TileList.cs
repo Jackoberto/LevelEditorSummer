ddd1fe8791796d66913a740cbf72eec45587bf24
Jackoberto <jackoberto01@gmail.com>
2021-07-05 17:21:53 +0200

    Adds TileList to store tiles in scriptable objects

---
+++ Assets/Scripts/TileList.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+[CreateAssetMenu(menuName = "LevelEditor/TileList")]
+public class TileList : ScriptableObject
+{
+    [SerializeField] private Tile[] tiles;
+    public Tile[] Tiles => tiles;
+    public Tile this[int index] => Tiles[index];
+}
