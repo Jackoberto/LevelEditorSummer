d99abfc085d14354e5e8768329e3061292fa611a
Jackoberto <jackoberto01@gmail.com>
2021-07-22 16:41:59 +0200

    Adds move tool

---
+++ Assets/Scripts/MoveMode.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+
+public class MoveMode : MonoBehaviour, IEditorMode
+{
+    private PreviousPoint previousPoint;
+    private Camera mainCamera;
+    private Editor editor;
+    private Transform objectToMove;
+    public string Name => "Move Tool";
+
+    public void Initialize(Editor editor)
+    {
+        mainCamera = Camera.main;
+        this.editor = editor;
+    }
+
+    public void Enter()
+    {
+        
+    }
+
+    public void Exit()
+    {
+        
+    }
+
+    public void EditorUpdate()
+    {
+        var mousePos = Input.mousePosition;
+        mousePos.z = mainCamera.transform.position.z * -1;
+        var ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        var worldPoint = ray.GetPoint(-ray.origin.z / ray.direction.z);
+        var overUI = editor.IsPointerOverUI();
+        var tilePlacingMode = editor.ValidMousePosition(Camera.main.ScreenToViewportPoint(Input.mousePosition));
+        if (overUI || !tilePlacingMode)
+            return;
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            var rayOrigin = mainCamera.ScreenToWorldPoint(mousePos);
+            var hitInfo = Physics2D.Raycast(rayOrigin,Vector2.zero);
+            if (hitInfo)
+            {
+                Debug.Log(hitInfo.transform.gameObject.name);
+            }
+            if (hitInfo.transform != null && hitInfo.transform.gameObject.TryGetComponent<EditorPrefabVisual>(out var prefabVisual))
+            {
+                previousPoint = new PreviousPoint(worldPoint, hitInfo.transform);
+            }
+        }
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if (previousPoint != null)
+            {
+                var pointDelta = worldPoint - previousPoint.Point;
+                previousPoint.Transform.position += pointDelta;
+                previousPoint.Point = worldPoint;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Mouse0))
+        {
+            previousPoint = null;
+        }
+    }
+}
+
+public class PreviousPoint
+{
+    public Vector3 Point;
+    public Transform Transform;
+    public PreviousPoint(Vector3 point, Transform transform)
+    {
+        Point = point;
+        Transform = transform;
+    }
+}
